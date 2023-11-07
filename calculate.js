b25a8979b2e789d199bbbcff9e687f3ec44f7c19
bmoki <breno26sonda@gmail.com>
2023-11-07 19:30:03 -0300

    feat: added javascript

---
+++ calculate.js
@@ -0,0 +1,9 @@
+let inputElementOne = document.getElementById("num1")
+let inputElementTwo = document.getElementById("num2")
+
+function SomaNumeros() {
+  let n1 = Number(inputElementOne.value);
+  let n2 = Number(inputElementTwo.value);
+
+  alert(n1 + n2)
+}
